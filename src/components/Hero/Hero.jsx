4646ed0ec633007ba6862392f57974ee43122199
MarcinK <marcin.kulbicki@gmail.com>
2023-11-24 14:57:34 +0100

    refactor(SectionHeading) add dynamic font size change

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,15 @@
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import SectionHeading from '../SectionHeading';
 import Categories from '../Categories';
 
 function Hero({ sectionName, categoriesArr, variant = 'light' }) {
     return (
-        <div>
+        <StyledHero>
             <SectionHeading>{sectionName.toUpperCase()}</SectionHeading>
             <Categories categories={categoriesArr} variant={variant} />
-        </div>
+        </StyledHero>
     );
 }
 
@@ -21,5 +22,9 @@ Hero.propTypes = {
     variant: PropTypes.string,
     categoriesArr: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
+
+const StyledHero = styled.div`
+    padding: 1rem 0;
+`;
 
 export default Hero;
--- src/components/SectionHeading/SectionHeading.jsx
+++ src/components/SectionHeading/SectionHeading.jsx
@@ -9,15 +9,26 @@ SectionHeading.propTypes = {
     children: PropTypes.node.isRequired,
 };
 
+// tutaj się za dużo dzieje
+// nie wiedziałem, jak dynamicznie ustawić h1
+// żeby zajmował jak najwięcej miejsca
 const StyledH1 = styled.h1`
     font-family: var(--font-extra-bold);
     letter-spacing: 5px;
-    font-size: clamp(2rem, 15vw, 13rem);
+    font-size: 11rem;
+    font-size: 17.5vw;
     text-align: center;
     margin-bottom: 0;
 
+    @media ${({ theme }) => theme.media.tablet} {
+        font-size: 5.5rem;
+        font-size: 16vw;
+    }
+
     @media ${({ theme }) => theme.media.mobile} {
         padding: 1rem 0;
+        font-size: 3.5rem;
+        font-size: 15.5vw;
     }
 `;
 
